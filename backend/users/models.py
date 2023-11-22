968b56a353ca756ecc1d54138ebffd738c0171a0
dev124816 <mghari.963@gmail.com>
2023-11-22 20:02:47 +0100

    .

--- backend/users/models.py
+++ backend/users/models.py
@@ -6,6 +6,6 @@ class User(AbstractBaseUser):
     username = models.CharField(max_length=100, unique=True, blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []  
